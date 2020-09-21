9198cfb95a04a353f3cc0c6ba24bb5d61618c1e5
Adam Staveley <adam.staveley@energyweb.org>
2020-09-21 17:21:45 +0200

    add create and resolve users did scripts

--- scripts/identity/create_user_dids.js
+++ scripts/identity/create_user_dids.js
@@ -1,9 +1,50 @@
-const { Wallet } = require('@ethersproject/wallet')
-const { address1056, abi1056, ProviderTypes, IResolverSettings, Resolver, IServiceEndpoint } = require('@ew-did-registry/did-ethr-resolver');
-const { users, devices } = require('./identities.json')
+const { writeFileSync } = require('fs')
+const { join } = require('path')
+const { address1056, abi1056, Operator } = require('@ew-did-registry/did-ethr-resolver');
+const { DIDDocumentFull } = require('@ew-did-registry/did-document')
+const { ProviderTypes } = require('@ew-did-registry/did-resolver-interface');
+const { Keys } = require('@ew-did-registry/keys')
+const { users } = require('./identities.json');
 
-for (user of users) {
 
-    const wallet = new Wallet(user.privateKey)
-   
+const main = async () => {
+    const updatedUsers = [...users]
+    for ([index, user] of users.entries()) {
+    
+        if (user.did && user.did.startsWith('did:ethr:')) {
+            console.log(`Found ${user.did}, skipping...`)
+            continue
+        }
+        
+        const keys = new Keys({ privateKey: user.privateKey })
+        const addr = keys.getAddress()
+        const did = `did:ethr:${addr}`
+    
+        const operator = new Operator(keys, {
+            provider: {
+                uriOrInfo: 'https://volta-rpc.energyweb.org',
+                type: ProviderTypes.HTTP
+            },
+            abi: abi1056,
+            address: address1056
+        })
+    
+        const document = new DIDDocumentFull(did, operator)
+        const created = await document.create()
+        if (created) {
+            console.log(`did created for ${user.role} ${addr}`)
+            updatedUsers[index].did = did
+        } else {
+            console.log(`unable to create ${did}`)
+        }
+    }
+    return updatedUsers
 }
+
+main()
+    .then(updatedUsers => writeFileSync(
+            join(__dirname, './identities.json'), 
+            JSON.stringify({ users: updatedUsers }, null, 2)
+        )
+    )
+    
---
+++ scripts/identity/resolve_dids.js
@@ -0,0 +1,28 @@
+const { address1056, abi1056, Operator, Resolver } = require('@ew-did-registry/did-ethr-resolver');
+const { ProviderTypes } = require('@ew-did-registry/did-resolver-interface');
+const { users } = require('./identities.json')
+
+const main = async () => {
+    const resolver = new Resolver({
+        provider: {
+            uriOrInfo: 'https://volta-rpc.energyweb.org',
+            type: ProviderTypes.HTTP
+        },
+        abi: abi1056,
+        address: address1056
+    })
+
+    const userDocuments = []
+    for (user of users) {
+        const document = await resolver.read(user.did)
+        delete user.privateKey
+        userDocuments.push({
+            ...user,
+            document
+        })
+    }
+    return userDocuments
+}
+
+main()
+    .then(documents => console.log(JSON.stringify(documents, null, 2)))
