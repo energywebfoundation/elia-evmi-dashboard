1cf55b510b4d6d9ac45237bcaa48f72b72d80f66
Adam Staveley <adam.staveley@energyweb.org>
2020-10-19 21:21:06 +0200

    deploy ev registry contract in docker dev network

---
+++ contracts/wait-for-node.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+curl_opts='--output /dev/null --silent --head --fail'
+
+until $(curl $curl_opts $OCN_NODE_URL/health); do
+    printf 'waiting for node up\n'
+    sleep 5
+done
+
+printf 'node is up'
+node deploy.js
